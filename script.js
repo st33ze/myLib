25654e17f04fdf593ab8f313e7d2925be2050a63
st33ze <st33ze@protonmail.com>
2021-11-18 16:32:33 +0100

    HTML - changed author input length CSS - decreased height of media query JS    - added form class    - form adds book to the firebase

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // CLASSES
-
-class Library {
-    constructor() {
-        // this.node = document.getElementById("books-list");
+class App {
+    static start() {
+        // Add event listeners.
+        document.querySelector(".login").addEventListener("click", () => {
+            firebase.auth().signInWithPopup(provider);
+        });
+        document.querySelector(".logout").addEventListener("click", () => {
+            firebase.auth().signOut().then(() => {
+                Library.clear();
+            }).catch(error => console.log(error));  
+        });
+        Menu.addListeners();
+        Form.addListeners();
     }
-    
+}
+class Library {
     static load(books) {
         // Gets books list as argument from firebase, and displays them.
         for (const bookID in books) this.#addRow(books[bookID], bookID);
@@ -14,8 +24,9 @@ class Library {
     static clear() {
         const table = document.querySelector("tbody");
         while (table.firstChild) table.removeChild(table.firstChild);
+        this.selectedBook = null;
+        Menu.fold();
         this.#updateView();
-
     }
 
     static #updateView() {
@@ -24,11 +35,25 @@ class Library {
         if (rows) {
             document.querySelector("main").style.justifyContent = "normal";
             document.getElementById("books-list").classList.remove("hidden");
+            Menu.node.style.alignSelf = "flex-end";
+            Menu.node.querySelector(".success").classList.remove("button-large");
+            Menu.node.classList.remove("hidden");
         } else {
             document.querySelector("main").style.justifyContent = "center";
             document.getElementById("books-list").classList.add("hidden");
+            Menu.node.classList.add("hidden");
+            Menu.node.style.alignSelf = "center";
+            Menu.node.querySelector(".success").classList.remove("button-large");
+            
         }
     }
+    static addBook(title, author, pages, read) {
+        const book = new Book(title, author, pages, read);
+        const bookRef = database.ref(`users/${App.user.uid}`).push(book);
+        this.#addRow(book, bookRef.key);
+        this.#updateView();
+    }
+
 
     static #addRow(book, id) {
         const row = document.querySelector("tbody").insertRow();
@@ -48,14 +73,14 @@ class Library {
                 this.selectedBook.classList.remove("selected");
                 if (this.selectedBook.dataset.id === row.dataset.id) {
                     this.selectedBook = null;
-                    // this.parent.menu.hide();
+                    Menu.fold();
                     return;
                 }
             }
             row.classList.add("selected");
             this.selectedBook = row;
             // if (this.parent.form.visible && this.parent.form.mode === "edit") this.parent.form.fill();
-            // this.parent.menu.show();
+            Menu.expand();
         })
     }
     
@@ -72,7 +97,271 @@ class Book {
 }
 
 
+class Menu {
+    static node = document.getElementById("menu");
+    static #addButton = this.node.querySelector(".success");
+    static #editButton = this.node.querySelector(".info");
+    static #deleteButton = this.node.querySelector(".warning");
 
+    static expand() {
+        this.#editButton.style.display = "flex";
+        this.#deleteButton.style.display = "flex";
+    }
+
+    static fold() {
+        this.#editButton.style.display = "none";
+        this.#deleteButton.style.display = "none";
+    }
+
+    static addListeners() {
+        this.#addButton.addEventListener("click", e => Form.show(e));
+    }
+}
+
+
+class Form {
+    static node = document.querySelector("form");
+    static #inputs = Array.from(this.node.querySelectorAll("input[type='text']"));
+    static #validationWarning = document.getElementById("validation-warning");
+
+    static addListeners() {
+        // Inputs styling.
+        this.#inputs.forEach(input => {
+            input.style.fontSize = "0.7rem";
+            input.addEventListener("focusin", e => {
+                input.previousElementSibling.style.fontSize = "0.7rem";
+                input.style.fontSize = "1rem";
+            });
+            input.addEventListener("focusout", e => {
+                if (!input.value) {
+                    input.previousElementSibling.style.fontSize = "1rem";
+                    input.style.fontSize = "0.7rem";
+                }
+            });
+        });
+
+        // Input validation.
+        this.#inputs.forEach(input => {
+            input.addEventListener("input", e => this.#validateInput(input));
+        });
+
+        // Cancel button.
+        this.node.querySelector(".warning").addEventListener("click", () => this.hide());
+
+        // Form submit.
+        this.node.addEventListener("submit", e => {
+            e.preventDefault();
+            // Validate text inputs.
+            if(!this.#inputs.map(input => this.#validateInput(input))
+                            .reduce((accumulator, currentValue) => accumulator && currentValue)) 
+                return;
+            
+            const title = this.node.elements[0].value;
+            const author = this.node.elements[1].value;
+            const pages = this.node.elements[2].value;
+            const read = this.node.elements[3].checked;
+    
+            if (this.mode === "add") 
+                Library.addBook(title, author, pages, read);
+            else if (this.mode === "edit") 
+                Library.editBook(title, author, pages, read);
+                
+            this.hide();
+        });
+    }
+
+    static show(e) {
+        Menu.node.classList.add("hidden");
+        this.node.classList.remove("hidden");
+        this.mode = e.srcElement.innerText;
+    }
+
+    static #validateInput(input) {
+        const patterns = {
+            title: /^[\p{L}\d,:\-']+$/u,
+            author: /^[\p{L}\.\-,']+$/u,
+            pages: /^\d+$/
+        }
+
+        let text = input.value.replaceAll(/\s\s*/g, " ");
+        if (patterns[input.id].test(text.replaceAll(/\s/g, ""))) {
+            input.classList.remove("invalid");
+            for (let i = 0; i < this.#inputs.length; i++) 
+                if (this.#inputs[i].classList.contains("invalid")) return true;
+            this.#validationWarning.style.visibility = "hidden";
+            return true;
+        } else {
+            input.classList.add("invalid");
+            this.#validationWarning.style.visibility = "visible";
+            return false;
+        } 
+    }
+}
+
+
+// function Form(parent) {
+//     this.parent = parent;
+//     this.node = document.querySelector("form");
+//     this.visible = false;
+//     // Components.
+//     this.inputs = Array.from(this.node.querySelectorAll("input[type='text']"));
+//     this.checkbox = this.node.querySelector(".checkbox-read input");
+//     this.saveButton = this.node.querySelector(".success");
+//     this.cancelButton = this.node.querySelector(".warning");
+//     this.validationWarning = document.getElementById("validation-warning");
+
+//     // Inputs styling.
+//     this.inputs.forEach(input => {
+//         input.style.fontSize = "0.7rem";
+//         input.addEventListener("focusin", e => {
+//             input.previousElementSibling.style.fontSize = "0.7rem";
+//             input.style.fontSize = "1rem";
+//         })
+//         input.addEventListener("focusout", e => {
+//             if (!input.value) {
+//                 input.previousElementSibling.style.fontSize = "1rem";
+//                 input.style.fontSize = "0.7rem";
+//             }
+//         })
+//     })
+
+//     // Event listeners.
+//     this.cancelButton.addEventListener("click", () => this.hide());
+
+//     this.inputs.forEach(input => {
+//         input.addEventListener("input", e => this.validateInput(input));
+//     });
+    
+//     this.node.addEventListener("submit", e => {
+//         e.preventDefault();
+//         // Validate text inputs.
+//         if(!this.inputs.map(input => this.validateInput(input))
+//                        .reduce((accumulator, currentValue) => accumulator && currentValue)) 
+//             return;
+        
+//         const title = this.node.elements[0].value;
+//         const author = this.node.elements[1].value;
+//         const pages = this.node.elements[2].value;
+//         const read = this.node.elements[3].checked;
+
+//         if (this.mode === "add") this.parent.library.addBook(title, author, pages, read);
+//         else if (this.mode === "edit") this.parent.library.editBook(title, author, pages, read);
+         
+//         this.hide();
+//     })
+
+//     // Other functions.
+//     this.show = function(e) {
+//         this.parent.showComponent(this.parent.menu, false);
+//         this.parent.showComponent(this, true);
+//         this.mode = e.srcElement.innerText;
+//     }
+
+//     this.hide = function() {
+//         this.parent.showComponent(this, false);
+//         this.parent.showComponent(this.parent.menu, true);
+//         this.node.reset();
+//         // Reset inputs styles.
+//         this.inputs.forEach(input => {
+//             input.classList.remove("invalid");
+//             input.previousElementSibling.style.fontSize = "1rem";
+//             input.style.fontSize = "0.7rem";
+//             this.validationWarning.style.visibility = "hidden";
+//         })
+//     }
+
+//     this.fill = function() {
+//         const book = this.parent.library.selectedBook;
+//         this.inputs.forEach((input, index) => {
+//             input.previousElementSibling.style.fontSize = "0.7rem";
+//             input.style.fontSize = "1rem";
+//             input.value = book.children[index].innerText;
+//         })
+//         this.checkbox.checked = book.classList.contains("read");
+//     }
+
+//     this.validateInput = function(input) {
+//         const patterns = {
+//             title: /^[\p{L}\d,:\-']+$/u,
+//             author: /^[\p{L}\.\-']+$/u,
+//             pages: /^\d+$/
+//         }
+
+//         let text = input.value.replaceAll(/\s\s*/g, " ");
+//         if (patterns[input.id].test(text.replaceAll(/\s/g, ""))) {
+//             input.classList.remove("invalid");
+//             for (let i = 0; i < this.inputs.length; i++) 
+//                 if (this.inputs[i].classList.contains("invalid")) return true;
+//             this.validationWarning.style.visibility = "hidden";
+//             return true;
+//         } else {
+//             input.classList.add("invalid");
+//             this.validationWarning.style.visibility = "visible";
+//             return false;
+//         } 
+//     }
+// }
+
+
+// Initialize Firebase.
+const firebaseConfig = {
+    apiKey: "AIzaSyAbBsL1v4wVaxC76GW947KFwyKaejv4Xzk",
+    authDomain: "library-45465.firebaseapp.com",
+    projectId: "library-45465",
+    storageBucket: "library-45465.appspot.com",
+    messagingSenderId: "576876775408",
+    appId: "1:576876775408:web:e9a269eef36814b804bf35",
+    measurementId: "G-HW2BGF3GP5"
+}
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// Firebase Authentication.
+const provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().useDeviceLanguage();
+firebase.auth().onAuthStateChanged(user => {
+    App.user = user;
+    if (user) {
+        // Log in.
+        // Show / hide components.
+        document.querySelector(".login").classList.add("hidden");
+        document.querySelector(".logout").classList.remove("hidden");
+        // document.getElementById("menu").classList.remove("hidden");
+        // Update header styles.
+        document.querySelector("header").style.justifyContent = "space-between";
+        // login();
+        database.ref().child("users").child(user.uid).get().then(snapshot => {
+            if (snapshot.exists()) Library.load(snapshot.val());
+        })
+    } else {
+        // Log out.
+        // Show / hide components.
+        document.querySelector(".login").classList.remove("hidden");
+        document.querySelector(".logout").classList.add("hidden");
+        // Update header styles.
+        document.querySelector("header").style.justifyContent = "flex-end";   
+    }
+});
+
+App.start();
+
+
+// function login() {
+//     // Show / hide components.
+//     document.querySelector(".login").classList.add("hidden");
+//     document.querySelector(".logout").classList.remove("hidden");
+//     // document.getElementById("menu").classList.remove("hidden");
+//     // Update header styles.
+//     document.querySelector("header").style.justifyContent = "space-between";
+// }
+
+// function logout() {
+//     // Show / hide components.
+//     document.querySelector(".login").classList.remove("hidden");
+//     document.querySelector(".logout").classList.add("hidden");
+//     // Update header styles.
+//     document.querySelector("header").style.justifyContent = "flex-end";
+// }
 
 
 
@@ -238,161 +527,3 @@ class Book {
 //     });
     
 // }
-
-// function Form(parent) {
-//     this.parent = parent;
-//     this.node = document.querySelector("form");
-//     this.visible = false;
-//     // Components.
-//     this.inputs = Array.from(this.node.querySelectorAll("input[type='text']"));
-//     this.checkbox = this.node.querySelector(".checkbox-read input");
-//     this.saveButton = this.node.querySelector(".success");
-//     this.cancelButton = this.node.querySelector(".warning");
-//     this.validationWarning = document.getElementById("validation-warning");
-
-//     // Inputs styling.
-//     this.inputs.forEach(input => {
-//         input.style.fontSize = "0.7rem";
-//         input.addEventListener("focusin", e => {
-//             input.previousElementSibling.style.fontSize = "0.7rem";
-//             input.style.fontSize = "1rem";
-//         })
-//         input.addEventListener("focusout", e => {
-//             if (!input.value) {
-//                 input.previousElementSibling.style.fontSize = "1rem";
-//                 input.style.fontSize = "0.7rem";
-//             }
-//         })
-//     })
-
-//     // Event listeners.
-//     this.cancelButton.addEventListener("click", () => this.hide());
-
-//     this.inputs.forEach(input => {
-//         input.addEventListener("input", e => this.validateInput(input));
-//     });
-    
-//     this.node.addEventListener("submit", e => {
-//         e.preventDefault();
-//         // Validate text inputs.
-//         if(!this.inputs.map(input => this.validateInput(input))
-//                        .reduce((accumulator, currentValue) => accumulator && currentValue)) 
-//             return;
-        
-//         const title = this.node.elements[0].value;
-//         const author = this.node.elements[1].value;
-//         const pages = this.node.elements[2].value;
-//         const read = this.node.elements[3].checked;
-
-//         if (this.mode === "add") this.parent.library.addBook(title, author, pages, read);
-//         else if (this.mode === "edit") this.parent.library.editBook(title, author, pages, read);
-         
-//         this.hide();
-//     })
-
-//     // Other functions.
-//     this.show = function(e) {
-//         this.parent.showComponent(this.parent.menu, false);
-//         this.parent.showComponent(this, true);
-//         this.mode = e.srcElement.innerText;
-//     }
-
-//     this.hide = function() {
-//         this.parent.showComponent(this, false);
-//         this.parent.showComponent(this.parent.menu, true);
-//         this.node.reset();
-//         // Reset inputs styles.
-//         this.inputs.forEach(input => {
-//             input.classList.remove("invalid");
-//             input.previousElementSibling.style.fontSize = "1rem";
-//             input.style.fontSize = "0.7rem";
-//             this.validationWarning.style.visibility = "hidden";
-//         })
-//     }
-
-//     this.fill = function() {
-//         const book = this.parent.library.selectedBook;
-//         this.inputs.forEach((input, index) => {
-//             input.previousElementSibling.style.fontSize = "0.7rem";
-//             input.style.fontSize = "1rem";
-//             input.value = book.children[index].innerText;
-//         })
-//         this.checkbox.checked = book.classList.contains("read");
-//     }
-
-//     this.validateInput = function(input) {
-//         const patterns = {
-//             title: /^[\p{L}\d,:\-']+$/u,
-//             author: /^[\p{L}\.\-']+$/u,
-//             pages: /^\d+$/
-//         }
-
-//         let text = input.value.replaceAll(/\s\s*/g, " ");
-//         if (patterns[input.id].test(text.replaceAll(/\s/g, ""))) {
-//             input.classList.remove("invalid");
-//             for (let i = 0; i < this.inputs.length; i++) 
-//                 if (this.inputs[i].classList.contains("invalid")) return true;
-//             this.validationWarning.style.visibility = "hidden";
-//             return true;
-//         } else {
-//             input.classList.add("invalid");
-//             this.validationWarning.style.visibility = "visible";
-//             return false;
-//         } 
-//     }
-// }
-
-
-// Initialize Firebase.
-const firebaseConfig = {
-    apiKey: "AIzaSyAbBsL1v4wVaxC76GW947KFwyKaejv4Xzk",
-    authDomain: "library-45465.firebaseapp.com",
-    projectId: "library-45465",
-    storageBucket: "library-45465.appspot.com",
-    messagingSenderId: "576876775408",
-    appId: "1:576876775408:web:e9a269eef36814b804bf35",
-    measurementId: "G-HW2BGF3GP5"
-}
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-
-// Firebase Authentication.
-const provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().useDeviceLanguage();
-firebase.auth().onAuthStateChanged(user => {
-    // app.user = user;
-    if (user) {
-        login();
-        database.ref().child("users").child(user.uid).get().then(snapshot => {
-            if (snapshot.exists()) Library.load(snapshot.val());
-        })
-    } else logout();
-});
-
-// Login, Logout event listeners.
-document.querySelector(".login").addEventListener("click", () => {
-    firebase.auth().signInWithPopup(provider)
-});
-
-document.querySelector(".logout").addEventListener("click", () => {
-    firebase.auth().signOut().then(() => {
-        Library.clear();
-    }).catch(error => console.log(error));  
-});
-
-
-function login() {
-    // Show / hide components.
-    document.querySelector(".login").classList.add("hidden");
-    document.querySelector(".logout").classList.remove("hidden");
-    // Update header styles.
-    document.querySelector("header").style.justifyContent = "space-between";
-}
-
-function logout() {
-    // Show / hide components.
-    document.querySelector(".login").classList.remove("hidden");
-    document.querySelector(".logout").classList.add("hidden");
-    // Update header styles.
-    document.querySelector("header").style.justifyContent = "flex-end";
-}
